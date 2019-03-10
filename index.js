093580a516f05ca938fdd04f7efdb3c55073d740
sashman <sashman90@gmail.com>
2019-03-10 21:14:18 +0000

    Useres query to backend api

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const { GraphQLServer } = require("graphql-yoga")
 const _ = require("lodash")
 const morgan = require("morgan")
-const PORT = process.env.PORT || 4000
+const axios = require("axios")
+const port = process.env.PORT || 3000
+const apiEndpoint = process.env.API_ENDPOINT || "http://localhost:4000"
 
 const resolvers = {
   Query: {
+    users: () =>
+      axios.get(`${apiEndpoint}/api/users`).then(({ data }) => data.data),
     user: (_, { id }) => {}
   }
   // Mutation: {}
 }
 
 const server = new GraphQLServer({
-  port: PORT,
   typeDefs: "./schema.graphql",
   resolvers
 })
 
 server.express.use(morgan("combined"))
-server.start(() =>
-  console.log(`GraphQL server is running on http://localhost:${PORT}`)
+server.start({ port }, () =>
+  console.log(`Graph#QL server is running on http://localhost:${port}`)
 )
